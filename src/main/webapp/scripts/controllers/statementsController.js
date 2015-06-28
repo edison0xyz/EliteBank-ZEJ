b3b41e18bd965916d9647a65dcc71a8f4e5f5a38
Zac <lervzc@gmail.com>
2015-06-28 21:42:55 +0800

    added demo pdf and statement gen

--- src/main/webapp/scripts/controllers/statementsController.js
+++ src/main/webapp/scripts/controllers/statementsController.js
@@ -1,7 +1,11 @@
 
 
-angular.module('eletrial').controller('StatementsController', function($scope ) {
-
+angular.module('eletrial').controller('StatementsController', function($scope, $timeout, $interval) {
+    function toLocal (date) {
+        var local = new Date(date);
+        local.setMinutes(date.getMinutes() - date.getTimezoneOffset());
+        return local.toJSON().slice(0, 10);
+    }
     $scope.datepicker = {
         options : {
             formatYear: 'yy',
@@ -23,19 +27,53 @@ angular.module('eletrial').controller('StatementsController', function($scope ) 
         $scope.end_opened = !$scope.end_opened;
     };
 
+    $scope.genLink = null;
+    $scope.genStatement = function(start,end){
+        $scope.genText = 'Generating statement from ' + toLocal(start) + 'to' + toLocal(end) + '.'
+        $interval(function(){
+            $scope.genText += '.';
+        }, 1000, 4);
 
+        $timeout(function(){
+            $scope.genLink = "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf";
+        }, 4500);
+
+
+    }
     $scope.statements = [
         {
-            title: 'P19023F Statement for',
-            labels: ["Real Estate", "Direct Investments", "Equities", "Bonds", "Commodities"],
-            date: new Date(),
-            type: 'Pie'
-        },
-        {
-            title: 'P58233F',
-            labels: ["Hedge funds", "Precious Metal", "Equities", "Bonds", "Commodities"],
-            data: [120, 500, 100, 40, 120],
-            type: 'Pie'
+            portfolio: 'P19023F',
+            month: "June",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
+        },{
+            portfolio: 'P19023F',
+            month: "May",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
+
+        },{
+            portfolio: 'P19023F',
+            month: "April",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
+        },{
+            portfolio: 'P19023F',
+            month: "March",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
+        },{
+            portfolio: 'P58233F',
+            month: "June",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
+        },{
+            portfolio: 'P58233F',
+            month: "May",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
+        },{
+            portfolio: 'P58233F',
+            month: "April",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
+        },{
+            portfolio: 'P58233F',
+            month: "March",
+            link: "generated/JIMMY SOH - SIN1798X - GENERATED 1_JUL_2015_1501H Sheet1.pdf"
         }
     ];
 });
